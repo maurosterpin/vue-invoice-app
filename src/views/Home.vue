761331b57f0f3edf7fc46f7da01fd39639d4340f
MSterpin <92162018+maurosterpin@users.noreply.github.com>
2021-11-01 23:30:28 +0100

    Invoice Filter

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,22 +4,25 @@
     <div class="header flex">
       <div class="left flex flex-column">
         <h1>Invoices</h1>
-        <span>There are 4 total invoices</span>
+        <span>There are {{ invoiceData.length }} total invoices</span>
       </div>
       <div class="right flex">
         <div @click="toggleFilterMenu" class="filter flex">
-          <span>Filter by status</span>
-          <img src="@/assets/icon-arrow-down.svg" alt="dropdownIcon" />
+          <span
+            >Filter by status
+            <span v-if="filteredInvoice">: {{ filteredInvoice }}</span></span
+          >
+          <img src="@/assets/icon-arrow-down.svg" alt="" />
           <ul v-show="filterMenu" class="filter-menu">
-            <li>Draft</li>
-            <li>Pending</li>
-            <li>Payed</li>
-            <li>Clear Filter</li>
+            <li @click="filteredInvoices">Draft</li>
+            <li @click="filteredInvoices">Pending</li>
+            <li @click="filteredInvoices">Paid</li>
+            <li @click="filteredInvoices">Clear Filter</li>
           </ul>
         </div>
         <div @click="newInvoice" class="button flex">
-          <div class="inner-button">
-            <img src="@/assets/icon-plus.svg" alt="addIcon" />
+          <div class="inner-button flex">
+            <img src="@/assets/icon-plus.svg" alt="" />
           </div>
           <span>New Invoice</span>
         </div>
@@ -28,7 +31,7 @@
     <!-- Invoices -->
     <div v-if="invoiceData.length > 0">
       <Invoice
-        v-for="(invoice, index) in invoiceData"
+        v-for="(invoice, index) in filteredData"
         :invoice="invoice"
         :key="index"
       />
@@ -44,13 +47,14 @@
 </template>
 
 <script>
-import Invoice from "../components/Invoice.vue";
+import Invoice from "../components/Invoice";
 import { mapMutations, mapState } from "vuex";
 export default {
   name: "Home",
   data() {
     return {
       filterMenu: null,
+      filteredInvoice: null,
     };
   },
   components: {
@@ -61,19 +65,43 @@ export default {
     newInvoice() {
       this.TOGGLE_INVOICE();
     },
+
     toggleFilterMenu() {
       this.filterMenu = !this.filterMenu;
+    },
+
+    filteredInvoices(e) {
+      if (e.target.innerText === "Clear Filter") {
+        this.filteredInvoice = null;
+        return;
+      }
+      this.filteredInvoice = e.target.innerText;
     },
   },
   computed: {
     ...mapState(["invoiceData"]),
+
+    filteredData() {
+      return this.invoiceData.filter((invoice) => {
+        if (this.filteredInvoice === "Draft") {
+          return invoice.invoiceDraft === true;
+        }
+        if (this.filteredInvoice === "Pending") {
+          return invoice.invoicePending === true;
+        }
+        if (this.filteredInvoice === "Paid") {
+          return invoice.invoicePaid === true;
+        }
+        return invoice;
+      });
+    },
   },
 };
 </script>
 
 <style lang="scss" scoped>
 .home {
-  color: white;
+  color: #fff;
 
   .header {
     margin-bottom: 65px;
@@ -87,24 +115,26 @@ export default {
       justify-content: flex-end;
       align-items: center;
 
-      .filter,
-      .button {
+      .button,
+      .filter {
         align-items: center;
 
         span {
           font-size: 12px;
         }
       }
+
       .filter {
-        cursor: pointer;
         position: relative;
         margin-right: 40px;
+        cursor: pointer;
 
         img {
           margin-left: 12px;
           width: 9px;
           height: 5px;
         }
+
         .filter-menu {
           width: 120px;
           position: absolute;
@@ -121,11 +151,12 @@ export default {
 
             &:hover {
               color: #1e2139;
-              background-color: white;
+              background-color: #fff;
             }
           }
         }
       }
+
       .button {
         padding: 8px 10px;
         background-color: #7c5dfa;
@@ -134,7 +165,7 @@ export default {
         .inner-button {
           margin-right: 8px;
           border-radius: 50%;
-          padding: 6px 10px;
+          padding: 8px;
           align-items: center;
           justify-content: center;
           background-color: #fff;
@@ -146,17 +177,21 @@ export default {
       }
     }
   }
+
   .empty {
     margin-top: 160px;
     align-items: center;
+
     img {
       width: 214px;
       height: 200px;
     }
+
     h3 {
       font-size: 20px;
       margin-top: 40px;
     }
+
     p {
       text-align: center;
       max-width: 224px;
